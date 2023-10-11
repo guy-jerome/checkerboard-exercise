3481a0c7f8604285f515620dd9dc3fbbaab93dd0
Aaron Roberts <aaronjamesroberts@gmail.com>
2023-10-11 13:59:11 -0500

    adds the basic checkerboard

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.createElement("div")
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "800px";
+container.style.height = "800px";
+//container.style.background = 'linear-gradient(to right, #ff0000, #00ff00)'
+
+for (let i = 1; i <= 8; i++){
+
+  for(let k = 1; k <=8; k++){
+    const tile = document.createElement("div")
+    tile.style.width = "12.5%"
+    tile.style.height = "12.5%"
+    if (i%2===0){
+      if (k%2===0){
+        tile.style.backgroundColor = "black"//rgba(0,0,0,0.5)"
+      }else{
+        tile.style.backgroundColor = "red"//rgba(255,0,0,0.5)"
+      }
+    }else{
+      if (k%2===0){
+        tile.style.backgroundColor = "red"//rgba(255,0,0,0.5)"
+      }else{
+        tile.style.backgroundColor = "black"//rgba(0,0,0,0.5)"
+      }
+    }
+    container.appendChild(tile)
+  }
+
+}
+document.body.appendChild(container);
