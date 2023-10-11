1246394d46d62be5c124bba907ff5d5644cc0df5
Aaron Roberts <aaronjamesroberts@gmail.com>
2023-10-11 14:50:18 -0500

    Adds a flashing with music version

--- script.js
+++ script.js
@@ -3,6 +3,51 @@ container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.width = "800px";
 container.style.height = "800px";
+container.style.margin = "20px";
+
+const audio = document.createElement("audio")
+audio.src = "trippy.mp3"
+
+const playButton = document.createElement("button")
+playButton.addEventListener("click",() => {audio.play()})
+
+const pauseButton = document.createElement("button")
+pauseButton.addEventListener("click",() => {audio.pause()})
+
+const restartButton = document.createElement("button")
+restartButton.addEventListener("click",() => {  audio.currentTime = 0;
+  audio.play();})
+
+document.body.style.backgroundColor = "black"
+document.body.style.display ="flex"
+document.body.style.flexDirection = "column"
+document.body.style.alignItems ="center"
+
+playButton.classList.add("button")
+pauseButton.classList.add("button")
+restartButton.classList.add("button")
+
+playButton.textContent = "Play"
+pauseButton.textContent = "Pause"
+restartButton.textContent = "Restart"
+
+document.body.appendChild(playButton)
+document.body.appendChild(pauseButton)
+document.body.appendChild(restartButton)
+
+const buttons = document.querySelectorAll(".button")
+
+buttons.forEach((button)=>{
+
+  button.style.backgroundColor = "rgb(150,20,120)"
+  button.style.width = "20vh"
+  button.style.height ="5vh"
+  button.style.marginLeft ="8vh"
+  button.style.marginRight ="8vh"
+  button.style.marginBottom ="1vh"
+  button.style.border = ".5vh solid black"
+  button.style.fontSize = "3vh"
+})
 
 function generateRandomColor(){
   
@@ -17,3 +62,12 @@ for (let i = 1; i <= 64; i++){
     container.appendChild(tile)
 }
 document.body.appendChild(container);
+
+function changeColor(){
+  for(let tile of container.children){
+    tile.style.backgroundColor = generateRandomColor()
+  }
+}
+
+setInterval(changeColor,2000)
+
